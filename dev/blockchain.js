3bb72385911c854202bac52f5f53367de4bfb59a
jpettett <joseph.pettett@gmail.com>
2021-02-26 18:56:35 -0800

    added hash block method. test returns sha256 hash

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require('sha256')
+
 //constructor function Blockchain
 function Blockchain() {
     this.chain = [];
@@ -38,6 +40,12 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) 
     
     return this.getLastBlock()['index'] + 1;
 
+}
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
 }
 
 module.exports = Blockchain
--- dev/test.js
+++ dev/test.js
@@ -2,16 +2,28 @@ const Blockchain = require('./blockchain')
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2380, 'ooddio908098', 'ugi839839220');
+// bitcoin.createNewBlock(2380, 'ooddio908098', 'ugi839839220');
 
-bitcoin.createNewTransaction(100, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
+// bitcoin.createNewTransaction(100, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
 
-bitcoin.createNewBlock(295080, '0908UJOUWHI', 'UDIH89U');
+// bitcoin.createNewBlock(295080, '0908UJOUWHI', 'UDIH89U');
 
-bitcoin.createNewTransaction(50, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
-bitcoin.createNewTransaction(300, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
-bitcoin.createNewTransaction(2000, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
+// bitcoin.createNewTransaction(50, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
+// bitcoin.createNewTransaction(300, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
+// bitcoin.createNewTransaction(2000, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
 
-bitcoin.createNewBlock(2444455080, '0908UJOUWHI', 'UDIH89U');
+// bitcoin.createNewBlock(2444455080, '0908UJOUWHI', 'UDIH89U');
 
-console.log(bitcoin.chain[2]);
+const previousBlockHash = 'OI91038038309802982039UEOIHOSUDH98';
+const currentBlockData = [
+    {
+    amount: 10,
+    sender: 'NDI09809809033J',
+    recipient: '0909I9U9OIUHSUISHSIU'
+}
+];
+
+const nonce = 100;
+
+
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
