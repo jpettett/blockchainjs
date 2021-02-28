b2ba5f12fe81ab5e98be49d60f24b335aec9de04
jpettett <joseph.pettett@gmail.com>
2021-02-28 13:29:15 -0800

    adds and tests proof of work method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -48,4 +48,16 @@ Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, n
     return hash;
 }
 
+//energy intensive proofOfWork method
+//return nonce with first 4 digits === 0000
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while (hash.substring(0, 4) !== '0000') {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+    return nonce;
+}
+
 module.exports = Blockchain
--- dev/test.js
+++ dev/test.js
@@ -2,28 +2,24 @@ const Blockchain = require('./blockchain')
 
 const bitcoin = new Blockchain();
 
-// bitcoin.createNewBlock(2380, 'ooddio908098', 'ugi839839220');
 
-// bitcoin.createNewTransaction(100, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
-
-// bitcoin.createNewBlock(295080, '0908UJOUWHI', 'UDIH89U');
-
-// bitcoin.createNewTransaction(50, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
-// bitcoin.createNewTransaction(300, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
-// bitcoin.createNewTransaction(2000, 'ALEX89DJSBJHBK', 'JEN3902874HUODHIU');
-
-// bitcoin.createNewBlock(2444455080, '0908UJOUWHI', 'UDIH89U');
-
-const previousBlockHash = 'OI91038038309802982039UEOIHOSUDH98';
+const previousBlockHash = '8JDHDIUEHEWIOUPWUO8493898080U0';
 const currentBlockData = [
     {
     amount: 10,
-    sender: 'NDI09809809033J',
+    sender: '888809809809033J',
+    recipient: '0909I9U9OIUHSUISHSIU'
+},
+{
+    amount: 30,
+    sender: 'NDIkdjdjdihwi09809809033J',
+    recipient: '0909I9U9OIUHSUISHSIU'
+},
+{
+    amount: 190,
+    sender: '90djjilwiwDI09809809033J',
     recipient: '0909I9U9OIUHSUISHSIU'
 }
 ];
 
-const nonce = 100;
-
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 106546))
