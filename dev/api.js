351ef811a7a2b4d7dd8a706e615436e2f85c3240
jpettett <joseph.pettett@gmail.com>
2021-03-01 19:53:22 -0800

    adds body-parser and tests first endpoint in postman

--- dev/api.js
+++ dev/api.js
@@ -1,12 +1,18 @@
+const bodyParser = require('body-parser');
 var express =require('express');
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.get('/blockchain', function (req, res) {
     res.send('Hello World')
 });
 
 app.post('/transaction', function(req, res) {
-
+    console.log(req.body);
+    res.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
 });
 
 app.get('/mine', function(req, res){
