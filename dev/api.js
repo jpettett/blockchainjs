d116f5c18b4a49195183f37b38421cf10fbd0231
jpettett <joseph.pettett@gmail.com>
2021-03-01 19:36:39 -0800

    adds initial code for get and post methods

--- dev/api.js
+++ dev/api.js
@@ -5,6 +5,15 @@ app.get('/blockchain', function (req, res) {
     res.send('Hello World')
 });
 
+app.post('/transaction', function(req, res) {
+
+});
+
+app.get('/mine', function(req, res){
+
+});
+
+
 
 app.listen(3000, function() {
     console.log('Listening on Port 3000...')
